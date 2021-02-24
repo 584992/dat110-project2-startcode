17153797ba4d28e6f854f71d8beddfc0b1b1f25b
Emma <ehellandhansen@gmail.com>
2021-02-24 18:26:44 +0100

    Update UnsubscribeMsg.java

--- src/no/hvl/dat110/messages/UnsubscribeMsg.java
+++ src/no/hvl/dat110/messages/UnsubscribeMsg.java
@@ -6,13 +6,34 @@ package no.hvl.dat110.messages;
  *
  */
 public class UnsubscribeMsg extends Message {
+	
+	//  sent by the client in order to unsubscribe from a topic
 
-	// message sent from client to unsubscribe on a topic 
+	// message sent from client to unsubscribe on a topic
 
 	// TODO:
 	// Implement object variables - a topic is required
 
+	String topic;
+
 	// Constructor, get/set-methods, and toString method
-	// as described in the project text
+	public UnsubscribeMsg(String topic, String user) {
+		super(MessageType.UNSUBSCRIBE, user);
+		this.topic = topic;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
 	
+	@Override
+	public String toString() {
+		return "Message [type=" + super.getType() + ", topic=" + topic +", user=" + super.getUser() + "]";
+	};
+
+
 }
