a72efd5ff12e7e644f3fd70c15386c73f9623c51
hvl584984 <54098979+hvl584984@users.noreply.github.com>
2021-02-24 18:39:54 +0100

    Update DeleteTopicMsg.java
    hehe

--- src/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ src/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -11,8 +11,25 @@ public class DeleteTopicMsg extends Message {
 
 	// TODO:
 	// Implement object variables - a topic is required
+	String topic;
+	
+	public DeleteTopicMsg(String user, String topic) {
+		super(MessageType.DELETETOPIC, user);
+		this.topic = topic;
+	}
 
 	// Constructor, get/set-methods, and toString method
 	// as described in the project text
+	public String getTopic() {
+		return topic;
+	}
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	
+	@Override
+	public String toString() {
+		return "Message [type=" + super.getType() + ", user=" + super.getUser() + " topic=" + topic + "]";
+	};
 
 }
