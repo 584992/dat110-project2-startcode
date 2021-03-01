84e6b13706a79b10e9328081743b5d5cda6db845
584992 <54098897+584992@users.noreply.github.com>
2021-03-01 11:39:39 +0100

    Update DisplayDevice.java

--- src/no/hvl/dat110/iotsystem/DisplayDevice.java
+++ src/no/hvl/dat110/iotsystem/DisplayDevice.java
@@ -8,8 +8,19 @@ import no.hvl.dat110.messages.Message;
 import no.hvl.dat110.messages.PublishMsg;
 import no.hvl.dat110.common.TODO;
 
+/**
+ * 
+ * @author vilde
+ *
+ */
 public class DisplayDevice {
 	
+	/**
+	 * It connects to the same broker as the sensor device, creates 
+	 * a 'temperature topic', subscribes to this topic and then recieves
+	 * the same number of messages as the sensor device is sending on
+	 * the topic. Then -> disconnect
+	 */
 	private static final int COUNT = 10;
 		
 	public static void main (String[] args) {
